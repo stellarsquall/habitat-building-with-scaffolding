613ef6f013e750647c220b88e891ec9984752821
Robin Beck <stellarsquall@protonmail.ch>
2020-07-08 01:10:29 -0600

    v0.2.0, obtain imagemagick pkg_deps from my origin

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -2,4 +2,4 @@ pkg_name=meme-machine
 pkg_origin=rab_tryhabitat
 pkg_version="0.2.0"
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_deps=( stellarsquall/imagemagick )
+pkg_deps=( stellarsquall_tryhabitat/imagemagick )
